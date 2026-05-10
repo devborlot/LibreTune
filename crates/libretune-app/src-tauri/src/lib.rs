6ae22b6258670a768d860e532c5c1719485cbc94
devborlot <devborlot@users.noreply.github.com>
2026-05-10 18:24:42 -0300

    Fix do cumpile and run at Mac Air M4 device, ans dependencies update to fix vunerabulitys. :)

--- crates/libretune-app/src-tauri/src/lib.rs
+++ crates/libretune-app/src-tauri/src/lib.rs
@@ -167,6 +167,7 @@ pub fn run() {
         .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .manage(AppState {
             connection: Mutex::new(None),
             definition: Mutex::new(None),
